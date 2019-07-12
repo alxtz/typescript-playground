784f0c46c43f95a74f46905d02cce12398f0c21d
Alex Tzeng <alxtz.tw@gmail.com>
2019-07-12 20:43:09 +0800

    feature: able to compile, watch and do type checking

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+type MatrixRow = number[];
+type Matrix = MatrixRow[];
+
+function diagonalDifference(matrix: Matrix): number {
+  let primaryDiagonalSum: number = 0;
+  let secondaryDiagonalSum: number = 0;
+
+  for (let i: number = 0; i < matrix.length; i++) {
+    primaryDiagonalSum += matrix[i][i];
+    secondaryDiagonalSum += matrix[i][matrix.length - i - 1];
+  }
+
+  return Math.abs(primaryDiagonalSum - secondaryDiagonalSum);
+}
+
+const a: number = "str"
+
+const exampleInput: Matrix = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+];
+console.log(diagonalDifference(exampleInput) === 15);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   entry: './src/index.ts',
-  mode: 'development'
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
